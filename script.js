21ba16ceccc01cb98387f55e0d246446424ebfba
jakmar86 <markalally@gmail.com>
2024-02-11 22:17:59 +0000

    update

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function getComputerChoice() {
     const computerChoice = ["rock","paper","scissors"];
     const random = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[random]
-
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -22,9 +21,78 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return "you win";
     } else {
-        return "invalid selection"
+        return "invalid selection";
     }
 }
+
+function playGame() {
+    let games == 0;
+    let player == 0;
+    let computer == 0;
+    if (playRound("rock", getComputerChoice()) == "you lose") {
+        games++;
+        computer++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "you win") {
+        games++;
+        player++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "draw") {
+        games++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    }
+    if (playRound("rock", getComputerChoice()) == "you lose") {
+        games++;
+        computer++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "you win") {
+        games++;
+        player++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "draw") {
+        games++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    }
+    if (playRound("rock", getComputerChoice()) == "you lose") {
+        games++;
+        computer++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "you win") {
+        games++;
+        player++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "draw") {
+        games++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    }
+    if (playRound("rock", getComputerChoice()) == "you lose") {
+        games++;
+        computer++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "you win") {
+        games++;
+        player++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "draw") {
+        games++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    }
+    if (playRound("rock", getComputerChoice()) == "you lose") {
+        games++;
+        computer++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "you win") {
+        games++;
+        player++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    } else if (playRound("rock", getComputerChoice()) == "draw") {
+        games++;
+        console.log(`Player ${player} - ${computer} Computer`);
+    }
+
+}
+
+
 
 const playerSelection = "rock"
 const computerSelection = getComputerChoice()
