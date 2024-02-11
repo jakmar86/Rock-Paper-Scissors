08112cffd377213e5429195238399ba4f6b976db
jakmar86 <markalally@gmail.com>
2024-02-11 21:55:11 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function getComputerChoice() {
+    const computerChoice = ["rock","paper","scissors"];
+    const random = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[random]
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        return "Draw";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "you lose";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "you win";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "win";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "you lose";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "you lose"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "you win";
+    } else {
+        return "invalid selection"
+    }
+
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection));
